7889be07c34443d536a3af1734792ed8c922dcb3
KirilPecev <kirilpecev@gmail.com>
2023-12-22 22:56:44 +0200

    Added new project for Domain classes

---
+++ Api/BtaApi/BtaApi.Domain/BtaDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BtaApi.Domain
+{
+    public class BtaDbContext : DbContext
+    {
+        public BtaDbContext(DbContextOptions<BtaDbContext> options) : base(options) { }
+    }
+}
---
+++ Api/BtaApi/BtaApi.Domain/Configuration/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BtaApi.Domain.Configuration
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services, IConfiguration configuration)
+            => services
+                .AddDbContext<BtaDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
